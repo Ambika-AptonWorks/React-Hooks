f1bb085162c9a320d661a495405a2d2ef57de8b8
Ambika <amika@aptonworks.com>
2022-03-28 15:54:25 +0530

    useCallback & useMemo

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Parent from './Parent';
+import Fact from './Fact';
+
+function App() {
+  return (
+    <div>
+        <Parent />
+        <Fact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Parent.js
@@ -0,0 +1,32 @@
+import React, {useState,useCallback} from 'react';
+import Title from './Title';
+import Count from './Count';
+import Button from './Button';
+
+
+function Parent(){
+    const [age,setAge]= useState(20)
+    const [salary,setSalary]= useState(40000)
+
+    const incrementAge=useCallback(()=> {
+        setAge(age+1)
+    },[age])
+
+    const incrementSalary=useCallback(()=> {
+        setSalary(salary+1000)
+    },[salary])
+
+
+    return(
+        <div>
+            <Title />
+            <Count text="age" count={age} />
+            <Button handleClick={incrementAge}>IncrementAge</ Button>
+            <Count text="salary" count={salary} />
+            <Button handleClick={incrementSalary}>IncrementSalary</Button>
+            <hr></hr>
+        </div>
+    )
+};
+
+export default Parent
