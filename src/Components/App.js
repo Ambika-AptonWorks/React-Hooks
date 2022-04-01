15bbdc088ac2e94f7111c8980e296e36377d14b4
Ambika <amika@aptonworks.com>
2022-04-01 15:51:01 +0530

    Calci

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,11 @@
-import React,{useRef} from 'react';
-import Parent from './Parent';
-import Fact from './Fact';
-import Counter from './Counter';
-import {useReactToPrint} from "react-to-print"
+import React from 'react';
+import Calculator from './Calculator';
 
 const App =()=> {
-  const componentRef=useRef();
-   const handlePrint = useReactToPrint({
-     content:() =>componentRef.current,
-   });
+ 
   return (
-    <div ref={componentRef}>
-        <Parent />
-        <Fact />
-        <Counter />
-        <button onClick={handlePrint}>Print</button>
+    <div>
+        <Calculator />
     </div>
   );
 }
